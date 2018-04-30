188ca172fda1b4a8047efac19d43e930610ad618
Nicholas Sladden <nickoliasxii@gmail.com>
2018-04-30 18:13:42 -0500

    fixed inline image link

--- src/CurrentlyReadingList.js
+++ src/CurrentlyReadingList.js
@@ -11,7 +11,7 @@ class CurrentlyReadingList extends Component {
         <li key={book.id}>
           <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks.thumbnail }}></div>
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
               <div className="book-shelf-changer">
                 <select>
                   <option value="none" disabled>Move to...</option>
